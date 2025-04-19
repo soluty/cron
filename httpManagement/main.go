a5d6e341b0b13d1880deecd611fcd5deddfdb3db
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-04-19 11:29:07 -0700

    improve management UI

--- httpManagement/main.go
+++ httpManagement/main.go
@@ -3,6 +3,7 @@ package httpManagement
 import (
 	"bytes"
 	"github.com/alaingilbert/cron"
+	"github.com/alaingilbert/cron/internal/utils"
 	"net/http"
 	"strconv"
 	"time"
@@ -12,27 +13,43 @@ func GetMux(c *cron.Cron) *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /{$}", getIndexHandler(c))
 	mux.HandleFunc("POST /{$}", postIndexHandler(c))
+	mux.HandleFunc("GET /{entryID}/{$}", getEntryHandler(c))
+	mux.HandleFunc("POST /{entryID}/{$}", postEntryHandler(c))
 	return mux
 }
 
-func getIndexHandler(c *cron.Cron) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var b bytes.Buffer
-		w.Header().Set("Content-Type", "text/html")
-		w.WriteHeader(http.StatusOK)
-		b.WriteString(`
+var css = `
 <style>
 	html {
 		background-color: #222;
 		color: #eee;
 		font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
 	}
+	form {
+		margin: 0;
+	}
 	table td {
 		padding: 0 5px;
 	}
-</style>`)
+	.monospace { font-family: monospace; }
+	a {
+		color: #eee;
+	}
+	.d-inline-block { display: inline-block; }
+	.active { color: #0f0; }
+	.inactive { color: #f00; }
+</style>
+`
+
+func getIndexHandler(c *cron.Cron) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var b bytes.Buffer
+		w.Header().Set("Content-Type", "text/html")
+		w.WriteHeader(http.StatusOK)
 		jobRuns := c.RunningJobs()
-		b.WriteString(`
+		entries := c.Entries()
+		b.WriteString(css + `
+
 Current time: ` + time.Now().Format(time.DateTime) + `<br />
 Running jobs (` + strconv.Itoa(len(jobRuns)) + `)<br />
 <table>
@@ -48,18 +65,77 @@ Running jobs (` + strconv.Itoa(len(jobRuns)) + `)<br />
 		for _, jobRun := range jobRuns {
 			b.WriteString(`
 	<tr>
-		<td>` + string(jobRun.Entry.ID) + `</td>
-		<td>` + string(jobRun.RunID) + `</td>
+		<td><span class="monospace"><a href="/` + string(jobRun.Entry.ID) + `">` + string(jobRun.Entry.ID) + `</a></span></td>
+		<td><span class="monospace">` + string(jobRun.RunID) + `</span></td>
 		<td>` + jobRun.Entry.Label + `</td>
 		<td>` + jobRun.StartedAt.Format(time.DateTime) + `</td>
 		<td>
-			<form method="POST" style="display: inline-block;">
+			<form method="POST" class="d-inline-block">
+				<input type="hidden" name="formName" value="cancelRun" />
 				<input type="hidden" name="entryID" value="` + string(jobRun.Entry.ID) + `" />
 				<input type="hidden" name="runID" value="` + string(jobRun.RunID) + `" />
 				<input type="submit" value="Cancel" />
 			</form>
 		</td>
 	</tr>`)
+		}
+		b.WriteString(`
+	</tbody>
+</table>
+
+<hr />
+
+Entries (` + strconv.Itoa(len(entries)) + `)<br />
+
+<table>
+	<thead>
+		<tr>
+			<th>Entry ID</th>
+			<th>Label</th>
+			<th>Prev</th>
+			<th>Next</th>
+			<th>Active</th>
+			<th>Actions</th>
+		</tr>
+	</thead>
+	<tbody>
+`)
+		for _, entry := range entries {
+			b.WriteString(`
+<tr>
+	<td><span class="monospace"><a href="/` + string(entry.ID) + `">` + string(entry.ID) + `</a></span></td>
+	<td>` + entry.Label + `</td>
+	<td>` + entry.Prev.Format(time.DateTime) + `</td>
+	<td>` + entry.Next.Format(time.DateTime) + `</td>
+	<td>` + utils.Ternary(entry.Active, `<span class="active">T</span>`, `<span class="inactive">F</span>`) + `</td>
+	<td>
+`)
+			if entry.Active {
+				b.WriteString(`
+		<form method="POST" class="d-inline-block">
+			<input type="hidden" name="formName" value="disableEntry" />
+			<input type="hidden" name="entryID" value="` + string(entry.ID) + `" />
+			<input type="submit" value="Disable" />
+		</form>
+`)
+			} else {
+				b.WriteString(`
+		<form method="POST" class="d-inline-block">
+			<input type="hidden" name="formName" value="enableEntry" />
+			<input type="hidden" name="entryID" value="` + string(entry.ID) + `" />
+			<input type="submit" value="Enable" />
+		</form>
+`)
+			}
+			b.WriteString(`
+		<form method="POST" class="d-inline-block">
+			<input type="hidden" name="formName" value="removeEntry" />
+			<input type="hidden" name="entryID" value="` + string(entry.ID) + `" />
+			<input type="submit" value="Remove" />
+		</form>
+	</td>
+</tr>
+`)
 		}
 		b.WriteString(`
 	</tbody>
@@ -71,10 +147,66 @@ Running jobs (` + strconv.Itoa(len(jobRuns)) + `)<br />
 
 func postIndexHandler(c *cron.Cron) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		entryID := cron.EntryID(r.PostFormValue("entryID"))
-		runID := cron.RunID(r.PostFormValue("runID"))
-		c.CancelRun(entryID, runID)
+		formName := r.PostFormValue("formName")
+		if formName == "cancelRun" {
+			entryID := cron.EntryID(r.PostFormValue("entryID"))
+			runID := cron.RunID(r.PostFormValue("runID"))
+			c.CancelRun(entryID, runID)
+		} else if formName == "removeEntry" {
+			entryID := cron.EntryID(r.PostFormValue("entryID"))
+			c.Remove(entryID)
+		} else if formName == "enableEntry" {
+			entryID := cron.EntryID(r.PostFormValue("entryID"))
+			c.Enable(entryID)
+		} else if formName == "disableEntry" {
+			entryID := cron.EntryID(r.PostFormValue("entryID"))
+			c.Disable(entryID)
+		}
 		w.Header().Set("Location", "/")
+		w.WriteHeader(http.StatusSeeOther)
+	}
+}
+
+func getEntryHandler(c *cron.Cron) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		entryID := cron.EntryID(r.PathValue("entryID"))
+		entry, err := c.Entry(entryID)
+		if err != nil {
+			w.Header().Set("Location", "/")
+			w.WriteHeader(http.StatusSeeOther)
+			return
+		}
+		var b bytes.Buffer
+		w.Header().Set("Content-Type", "text/html")
+		w.WriteHeader(http.StatusOK)
+		b.WriteString(css + `
+<a href="/">back</a><br />
+<h1>` + string(entry.ID) + `</h1>
+<form method="POST">
+	<input type="hidden" name="formName" value="updateLabel" />
+	<input type="text" name="label" value="` + entry.Label + `" />
+	<input type="submit" value="Update label" />
+</form>
+`)
+		_, _ = w.Write(b.Bytes())
+	}
+}
+
+func postEntryHandler(c *cron.Cron) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		entryID := cron.EntryID(r.PathValue("entryID"))
+		_, err := c.Entry(entryID)
+		if err != nil {
+			w.Header().Set("Location", "/")
+			w.WriteHeader(http.StatusSeeOther)
+			return
+		}
+		formName := r.PostFormValue("formName")
+		if formName == "updateLabel" {
+			label := r.PostFormValue("label")
+			c.UpdateLabel(entryID, label)
+		}
+		w.Header().Set("Location", "/"+string(entryID))
 		w.WriteHeader(http.StatusSeeOther)
 	}
 }
